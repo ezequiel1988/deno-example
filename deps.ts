2113186a1162c87ab9e896fe14c532ecbf8e8eea
ezequiel <ekiolivier@gmail.com>
2022-05-02 22:43:56 -0300

    delete oak imports

--- deps.ts
+++ deps.ts
@@ -1,7 +1,4 @@
-//export { Application, Router, Context, send } from "https://deno.land/x/oak/mod.ts";
 export { Application, type HandlerFunc, type MiddlewareFunc } from "https://deno.land/x/abc@v1.3.3/mod.ts";
-
-export type { RouterMiddleware } from "https://deno.land/x/oak/mod.ts";
 
 export { config } from "https://deno.land/x/dotenv/mod.ts";
 
