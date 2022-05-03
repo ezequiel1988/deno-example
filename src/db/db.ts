be95832a2025ba5d920888f7453828103abce9c6
ezequiel1988 <45713049+ezequiel1988@users.noreply.github.com>
2022-05-03 11:29:57 -0300

    Update db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,7 +3,7 @@ import { MongoClient } from '../../deps.ts'
 const client = new MongoClient();
 
     try {
-        await client.connect('mongodb+srv://admin:1q2w3e4r5t@example.oevro.mongodb.net/deno-db?authMechanism=SCRAM-SHA-1&retryWrites=true&w=majority');
+        await client.connect('');
         console.log('Connected to database');
         
     } catch (error) {
